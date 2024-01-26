e966f1cd0dcefc3cb7002352fd68ce8d5f73e0d7
Jan Hořava <czechinthemail@gmail.com>
2024-01-26 06:02:47 -0800

    updates, progress, and nyse countdown

---
+++ guides/investing/tools.js
@@ -0,0 +1,41 @@
+'use strict';
+
+function repo_init(){
+    core_repo_init({
+      'owner': 'honzi',
+      'root': '../../index.htm',
+      'title': 'honzi.github.io',
+    });
+
+    core_interval_modify({
+      'id': 'second',
+      'interval': 1000,
+      'sync': true,
+      'todo': update_time,
+    });
+}
+
+function update_time(){
+    const now = new Date();
+    const opening = new Date();
+    opening.setHours(
+      14 - now.getTimezoneOffset() / 60,
+      30,
+      0,
+      0
+    );
+    if(now > opening){
+        opening.setDate(opening.getDate() + 1);
+    }
+    const seconds = (opening - now) / 1000;
+
+    document.getElementById('time').innerHTML = core_digits_min({
+      'number': Number.parseInt(seconds / 3600, 10) % 24,
+    }) + ':'
+    + core_digits_min({
+      'number': Number.parseInt(seconds / 60, 10) % 60,
+    }) + ':'
+    + core_digits_min({
+      'number': Math.round(seconds % 60),
+    });
+}
